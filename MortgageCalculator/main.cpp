61a54d1f844d76979644ae42afbf6a0e58e3d3b9
Doug Ryan <ryan_djr@yahoo.com>
2021-12-22 15:01:03 -0800

    Add project files.

---
+++ MortgageCalculator/main.cpp
@@ -0,0 +1,18 @@
+//**********************************************************************
+// File:				main.cpp
+// Author:			
+// Date:				
+// Class:				Intro to C++
+// Assignment:	Assign 02 - Mortgage Calculator
+// Purpose:			
+// Hours:				
+//**********************************************************************
+
+#include <iostream>
+
+using namespace std;
+
+int main () {
+
+	return EXIT_SUCCESS;
+}
